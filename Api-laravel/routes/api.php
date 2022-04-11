349359f2480de9e2683b4fe881df5462f0ab0997
ThiagoWaldrich <thiagowlf7@gmail.com>
2022-04-11 15:44:42 -0300

    Test Working with Routes

--- Api-laravel/routes/api.php
+++ Api-laravel/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('Hello',function(){
+    return 'hello world';
+});
+Route::post('Hello-Post',function(){
+    return 'hello post';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
