c5b28d3ad635d9dcf42db515bd74ee80fb407769
ThiagoWaldrich <thiagowlf7@gmail.com>
2022-04-13 15:07:25 -0300

    Return a Json File

--- Api-laravel/app/Http/Controllers/HelloWorldController.php
+++ Api-laravel/app/Http/Controllers/HelloWorldController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HelloWorldController extends Controller{
 
     public function hello($name){
-        return 'Hello Controller ' . $name;
+        return response()->json(data:"Hello World {$name}");
     }
 
 }
