6e22dd8297f9ec4eeb63a622d7322a2383cbbfbf
ThiagoWaldrich <thiagowlf7@gmail.com>
2022-04-14 08:38:08 -0300

    add arrays with bands

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -6,7 +6,32 @@ use Illuminate\Http\Request;
 
 class BandController extends Controller
 {
-    public function getAll(){
-        return response()->json([]);
+    public function getAll()
+    {
+
+        $bands = $this->getBands();
+
+        return response()->json($bands);
+    }
+
+    protected function getBands()
+    {
+        return [
+            [
+                'id' => 1, 'name' => 'Angra', 'gender' => 'power metal'
+            ],
+            [
+                'id' => 2, 'name' => 'Shaman', 'gender' => 'power metal'
+            ],
+            [
+                'id' => 3, 'name' => 'Stratovarius', 'gender' => 'power metal'
+            ],
+            [
+                'id' => 4, 'name' => 'Viper', 'gender' => 'power metal'
+            ],
+            [
+                'id' => 5, 'name' => 'Edu Falaschi', 'gender' => 'power metal'
+            ]
+        ];
     }
 }
